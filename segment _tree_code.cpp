557d31646790f397abf05f33da6e2c12e840589a
sangam65 <77439991+sangam65@users.noreply.github.com>
2022-06-03 10:23:06 +0530

    Add files via upload

--- segment _tree_code.cpp
+++ segment _tree_code.cpp
@@ -1,4 +1,4 @@
-void build(int l,int h,int ind,int lt,int a[],int seg[])
+void build(int l,int h,int ind,int a[],int seg[])
 {
     if(l==h)
     {
@@ -8,17 +8,16 @@ void build(int l,int h,int ind,int lt,int a[],int seg[])
         
     }
     int m=(l+h)/2;
-    build(l,m,2*ind+1,lt+1,a,seg);
-    build(m+1,h,2*ind+2,lt+1,a,seg);
-    if(lt%2==0)
-    seg[ind]=seg[2*ind+1]|seg[2*ind+2];
-    else
-        seg[ind]=seg[2*ind+1]^seg[2*ind+2];
+    build(l,m,2*ind+1,a,seg);
+    build(m+1,h,2*ind+2,a,seg);
+   
+    seg[ind]=seg[2*ind+1]+seg[2*ind+2];
+   
     
 
 }
 
-          //  fixed l,h l and h are the query value given
+          
 int query(int l,int h,int low,int high,int ind,int seg[])
 {
     if(l>high||h<low)
@@ -29,10 +28,10 @@ int query(int l,int h,int low,int high,int ind,int seg[])
 
     int x=query(l,h,low,m,2*ind+1,seg);
     int y=query(l,h,m+1,high,2*ind+2,seg);
-    return min(x,y);
+    return x+y;
 }
 
-void update(int pt,int l,int h,int ind,int z,int lt,int seg[])
+void update(int pt,int l,int h,int ind,int z,int seg[])
 {
     if(l==h)
     {
@@ -44,16 +43,14 @@ void update(int pt,int l,int h,int ind,int z,int lt,int seg[])
     int m=(l+h)/2;
     if(m>=pt)
     {
-        update(pt,l,m,2*ind+1,z,lt+1,seg);
+        update(pt,l,m,2*ind+1,z,,seg);
     }
     else
     {
-        update(pt,m+1,h,2*ind+2,z,lt+1,seg);
+        update(pt,m+1,h,2*ind+2,z,seg);
     }
-    if(lt%2==0)
-    seg[ind]=seg[2*ind+1]|seg[2*ind+2];
-    else
-        seg[ind]=seg[2*ind+1]^seg[2*ind+2];
+    
+    seg[ind]=seg[2*ind+1]+seg[2*ind+2];
     
    
 }
